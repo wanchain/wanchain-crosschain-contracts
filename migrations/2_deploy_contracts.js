dc80dd4aabc1bbba55118082f4e99cdfd216cbde
Anson5555555Lee <frank5555555li@gmail.com>
2018-10-23 11:06:28 +0800

    remove irrelevant dirs

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var weth = artifacts.require("./WETH.sol");
-var wethManager = artifacts.require("./WETHManager.sol");
-var smgAdmin =  artifacts.require("./StoremanGroupAdmin.sol");
-var groupWETH =  artifacts.require("./HTLCWETH.sol");
-var groupETH =  artifacts.require("./HTLCETH.sol")
-
-module.exports = function(deployer,network, accounts) {
-
-};
