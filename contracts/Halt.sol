8a697b3f36065b68fd55f5f9d31128f2190824cc
Anson5555555Lee <frank5555555li@gmail.com>
2018-10-24 10:25:07 +0800

    add Halt.sol

---
+++ contracts/Halt.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.11;
+import './Owned.sol';
+
+contract Halt is Owned {
+    
+    bool public halted = true; 
+    
+    modifier notHalted() {
+        require(!halted);
+        _;
+    }
+
+    modifier isHalted() {
+        require(halted);
+        _;
+    }
+    
+    /// @notice function Emergency situation that requires 
+    /// @notice contribution period to stop or not.
+    function setHalt(bool halt) 
+        public 
+        onlyOwner
+    {
+        halted = halt;
+    }
+}
