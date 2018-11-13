2c1552f7f9f74a30ba6363c382630ab40e79831f
Anson5555555Lee <frank5555555li@gmail.com>
2018-11-13 12:30:21 +0800

    remove isContract method and add require verification

--- contracts/HTLCWAN.sol
+++ contracts/HTLCWAN.sol
@@ -206,7 +206,7 @@ contract HTLCWAN is HTLCBase {
         payable
         returns(bool) 
     {
-        require(!isContract(msg.sender));
+        require(tx.origin == msg.sender);
         require(TokenInterface(tokenManager).isTokenRegistered(tokenOrigAddr));
 
         // check withdraw fee
@@ -347,22 +347,5 @@ contract HTLCWAN is HTLCBase {
         storemanGroupAdmin = addr;
 
         return true;
-    }
-
-    /// @notice                 internal function to determine if an address is a contract
-    /// @param  addr            the address being queried
-    /// @return                 true if `addr` is a contract
-    function isContract(address addr) 
-        internal 
-        view 
-        returns(bool) 
-    {
-        uint size;
-        if (addr == 0) return false;
-        assembly {
-            size := extcodesize(addr)
-        }
-        
-        return size > 0;
     }
 }
