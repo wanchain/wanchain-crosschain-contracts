ce67744e646c220cbbbe1cacb6ee71da6b11e984
liao xingxing <38098634+13811065718@users.noreply.github.com>
2019-03-07 15:29:59 +0800

    update storemanGroup.sol
    to add a parameter for the smg debt transfer function

--- contracts/StoremanGroup.sol
+++ contracts/StoremanGroup.sol
@@ -35,9 +35,9 @@ contract StoremanGroup is Halt {
 
     ///the minumum deposit for each depositor which is reuqired by storeman group
     uint public constant    DEFAULT_PRECISE     = 10000;
-    uint public constant    MIN_DEPOSIT         =  1000000000000000000000;
-    uint public constant    MAX_COMMISION_BASE  =  5000000000000000000000;
-    uint public constant    MAX_DEPOSIT_SUM     = 30000000000000000000000; 
+    uint public constant    MIN_DEPOSIT         =    1000000000000000000000;
+    uint public constant    MAX_COMMISION_BASE  =    5000000000000000000000;
+    uint public constant    MAX_DEPOSIT_SUM     = 4000000000000000000000000; 
 
     ///the storeman group administration contract address
     address public      storemanGroupAdmin;
@@ -188,10 +188,10 @@ contract StoremanGroup is Halt {
     /// @notice                           event of exchange WERC20 token with ERC20 token request
     /// @param _objectSmgAddr             address of storemanGroup where debt will be transfered
     /// @param _tokenOrigAddr             address of ERC20 token
-    /// @param _locatedMpcAddr            address of current smg locked Mpc node
-    /// @param _xHash                     hash of random number x
+    /// @param _locatedMpcAddr            address of current smg locked Mpc eth address
+    /// @param xHash                     hash of random number x
     /// @param _debtValue                 debt value of which needed to transfer to _objectSmgAddr
-    event StoremanGroupDebtTransferLogger(address indexed _objectSmgAddr, address indexed _tokenOrigAddr, address indexed _locatedMpcAddr, bytes32 _xHash, uint256 _debtValue);
+    event StoremanGroupDebtTransferLogger(address indexed _objectSmgAddr, address indexed _tokenOrigAddr, address indexed _locatedMpcAddr, bytes32 xHash, uint256 _debtValue);
     
 
     /**
@@ -530,18 +530,19 @@ contract StoremanGroup is Halt {
     /// @notice                            function for storemanGroup applying unregister (needed to confirm further)
     /// @param _objectSmgAddr              address of smg which used to accept debt
     /// @param _tokenOrigAddr              address of token on original chain 
+    /// @param _locatedMpcEthAddr          address of MPC eth address 
     /// @param _xHash                      hash of random number x
     /// @param _value                      debt value that needed to transfer  
-    function applySmgDebtTransfer(address _objectSmgAddr, address _tokenOrigAddr, bytes32 _xHash, uint256 _value)
+    function applySmgDebtTransfer(address _objectSmgAddr, address _tokenOrigAddr, address _locatedMpcEthAddr, bytes32 _xHash, uint256 _value)
     public
     notHalted
     onlyOwner
     {
-        require((address(0) != locatedMpcAddr) && (address(0) != _objectSmgAddr) && (address(0) != _tokenOrigAddr), "Invalid address value!");
+        require((address(0) != _locatedMpcEthAddr) && (address(0) != _objectSmgAddr) && (address(0) != _tokenOrigAddr), "Invalid address value!");
         require((_value > 0) && (bytes32(0) != _xHash), "Invalid hash(X) or debt value, please check again!");
         require((now > runningEndTime) && (scStatus == SCStatus.Registered), "Do not transfer smg's debt right now");
 
-        emit StoremanGroupDebtTransferLogger(_objectSmgAddr, _tokenOrigAddr, locatedMpcAddr, _xHash, _value);     
+        emit StoremanGroupDebtTransferLogger(_objectSmgAddr, _tokenOrigAddr, _locatedMpcEthAddr, _xHash, _value);     
     }
 
     /// @notice                            function for storemanGroup applying unregister
