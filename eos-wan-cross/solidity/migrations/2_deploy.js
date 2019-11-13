bb99494debdf4e7043fec57bcf823182b39018f7
Gabriel Guo <pkguowu@gmail.com>
2019-11-13 18:11:41 +0800

    common proxy contract

--- eos-wan-cross/solidity/migrations/2_deploy.js
+++ eos-wan-cross/solidity/migrations/2_deploy.js
@@ -2,15 +2,15 @@ const TokenManagerProxy = artifacts.require('TokenManagerProxy');
 const TokenManagerDelegate = artifacts.require('TokenManagerDelegate');
 const QuotaLib = artifacts.require('QuotaLib');
 const HTLCLib = artifacts.require('HTLCLib');
-// const HTLCDelegate = artifacts.require('HTLCDelegate');
+const HTLCDelegate = artifacts.require('HTLCDelegate');
 
 module.exports = async (deployer) => {
-  // await deployer.deploy(TokenManagerProxy);
-  // await deployer.deploy(TokenManagerDelegate);
+  await deployer.deploy(TokenManagerProxy);
+  await deployer.deploy(TokenManagerDelegate);
   await deployer.deploy(QuotaLib);
   await deployer.deploy(HTLCLib);
 
-  // await deployer.link(QuotaLib, HTLCDelegate);
+  await deployer.link(QuotaLib, HTLCDelegate);
   
-  // await deployer.deploy(HTLCDelegate);
+  await deployer.deploy(HTLCDelegate);
 }
