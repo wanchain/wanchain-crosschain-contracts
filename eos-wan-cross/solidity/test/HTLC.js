49b04f37fd79899c255af52cfa434777dec8920e
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-12-02 16:25:08 +0800

    improve case coverage

--- eos-wan-cross/solidity/test/HTLC.js
+++ eos-wan-cross/solidity/test/HTLC.js
@@ -7,6 +7,7 @@ const HTLCLib               = artifacts.require('HTLCLib');
 const HTLCDebtLib           = artifacts.require('HTLCDebtLib');
 const HTLCSmgLib            = artifacts.require('HTLCSmgLib');
 const HTLCUserLib           = artifacts.require('HTLCUserLib');
+const Proxy                 = artifacts.require('Proxy');
 const HTLCProxy             = artifacts.require('HTLCProxy');
 const HTLCDelegate          = artifacts.require('HTLCDelegate');
 const StoremanGroupProxy    = artifacts.require('StoremanGroupProxy');
@@ -58,11 +59,22 @@ const xHash10        = '0x81b04ae4944e1704a65bc3a57b6fc3b06a6b923e3c558d611f6a85
 const x11            = '0x000000000000000000000000000000000000000000000000000000000000000b';
 const xHash11        = '0xc09322c415a5ac9ffb1a6cde7e927f480cc1d8afaf22b39a47797966c08e9c4b';
 
+// token info.
+let tokenInfo = {
+  decimals              : 18,
+  tokenOrigAccount      : web3.utils.hexToBytes(web3.utils.asciiToHex('Eos contract address')),
+  token2WanRatio        : 10000,                  // 1:1
+  minDeposit            : new BN(20).mul(new BN(10).pow(new BN(18))),
+  withdrawDelayTime     : 60 * 60 * 72,
+  name                  : web3.utils.hexToBytes(web3.utils.asciiToHex('Eos')),
+  symbol                : web3.utils.hexToBytes(web3.utils.asciiToHex('EOS'))
+};
 
-const v1            = 20;
-const v2            = 10;
-const v3            = 5;
-const appproveValue = 10;
+const v1            = new BN(20).mul(new BN(10).pow(new BN(tokenInfo.decimals)));
+const v2            = new BN(10).mul(new BN(10).pow(new BN(tokenInfo.decimals)));
+const v3            = new BN(5).mul(new BN(10).pow(new BN(tokenInfo.decimals)));
+const v4            = new BN(2).mul(new BN(10).pow(new BN(tokenInfo.decimals)));
+const appproveValue = new BN(20).mul(new BN(10).pow(new BN(tokenInfo.decimals)));
 
 const storemanPK1   = '0x047a5380730dde59cc2bffb432293d22364beb250912e0e73b11b655bf51fd7a8adabdffea4047d7ff2a9ec877815e12116a47236276d54b5679b13792719eebb9';
 const txFeeRatio1    = '20';
@@ -73,24 +85,16 @@ const txFeeRatio2   = '20';
 
 const srcDebtStoremanPK   = '0x047a5380730dde59cc2bffb432293d22364beb250912e0e73b11b655bf51fd7a8adabdffea4047d7ff2a9ec877815e12116a47236276d54b5679b13792719eebbb';
 const dstDebtStoremanPK   = '0x047a5380730dde59cc2bffb432293d22364beb250912e0e73b11b655bf51fd7a8adabdffea4047d7ff2a9ec877815e12116a47236276d54b5679b13792719eebbc';
-const srcDebtStoremanPK1   = '0x047a5380730dde59cc2bffb432293d22364beb250912e0e73b11b655bf51fd7a8adabdffea4047d7ff2a9ec877815e12116a47236276d54b5679b13792719eebbd';
+const srcDebtStoremanPK1  = '0x047a5380730dde59cc2bffb432293d22364beb250912e0e73b11b655bf51fd7a8adabdffea4047d7ff2a9ec877815e12116a47236276d54b5679b13792719eebbd';
 
-const R             = '0x042c73b8cbc70bb635922a60f5eb9e6dcae637bbb05869fa5a4134f0c5ec859c0462696cd577c419666045ec6310a85f6638532d8d23cdbc2006a60dc3fbbada7e';
-const s             = '0x0c595b48605562a1a6492540b875da4ff203946a9dd0e451cd33d06ef568626b';
+const R                   = '0x042c73b8cbc70bb635922a60f5eb9e6dcae637bbb05869fa5a4134f0c5ec859c0462696cd577c419666045ec6310a85f6638532d8d23cdbc2006a60dc3fbbada7e';
+const s                   = '0x0c595b48605562a1a6492540b875da4ff203946a9dd0e451cd33d06ef568626b';
 
-const ADDRESS_0     = '0x0000000000000000000000000000000000000000';
-const ADDRESS_TM     = '0x0000000000000000000000000000000000000001';
-const ADDRESS_SMGADMIN     = '0x0000000000000000000000000000000000000002';
-// token info.
-let tokenInfo = {
-  tokenOrigAccount      : web3.utils.hexToBytes(web3.utils.asciiToHex('Eos contract address')),
-  token2WanRatio        : 10000,                  // 1:1
-  minDeposit            : "10000000000000000000", // 10*10^18
-  withdrawDelayTime     : 60 * 60 * 72,
-  name                  : web3.utils.hexToBytes(web3.utils.asciiToHex('Eos')),
-  symbol                : web3.utils.hexToBytes(web3.utils.asciiToHex('EOS')),
-  decimals              : 18
-};
+const ADDRESS_0           = '0x0000000000000000000000000000000000000000';
+const ADDRESS_TM          = '0x0000000000000000000000000000000000000001';
+const ADDRESS_SMGADMIN    = '0x0000000000000000000000000000000000000002';
+const ADDRESS_HTLC_PROXY_IMPL     = '0x0000000000000000000000000000000000000003';
+
 
 let htlcSmgLockParams = {
   tokenOrigAccount: tokenInfo.tokenOrigAccount,
@@ -203,8 +207,6 @@ contract('Test HTLC', async (accounts) => {
   it('init...   -> getStoremanFee success', async() => {
     try{
       let stmFee = await htlcInstProxy.getStoremanFee(addSmgParams.storemanGroupPK);
-      //console.log(new BN(stmFee).toString());
-      //console.log(new BN(addSmgParams.txFeeRatio).toString());
       assert.equal(new BN(stmFee).eq(new BN(0)),true);
     }catch(err){
       assert.fail(err.toString());
@@ -222,10 +224,6 @@ contract('Test HTLC', async (accounts) => {
       let bnDefultPrecise = new BN(DEFAULT_PRECISE);
       let value = new BN(addSmgParams.quota);
       let quato = value.mul(bnDefultPrecise).mul(bnUnitToken).div(bnToken2WanRation).div(bnUnitEth).mul(new BN(2));
-
-      // console.log(new BN(ret[0]).toString());
-      // console.log(quato.toString());
-
       assert.equal(new BN(ret[0]).eq(new BN(quato)),true);
     }catch(err){
       assert.fail(err.toString());
@@ -266,6 +264,108 @@ contract('Test HTLC', async (accounts) => {
       assert.include(err.toString(),"Value must be greater than minDeposit");
     }
   });
+
+  let tmAddress, smgAddress, precise;
+  it('Others getEconomics  ==>The default value', async() => {
+    try{
+      let ret = await htlcInstProxy.getEconomics();
+      tmAddress = ret[0];
+      smgAddress = ret[1];
+      precise = ret[2];
+      assert.notEqual(ret[0], ADDRESS_0);
+      assert.notEqual(ret[1], ADDRESS_0);
+      assert.equal(ret[2].toNumber(), 0);
+    }catch(err){
+      assert.fail(err.toString());
+    }
+  });
+
+  it('Others setEconomics  ==>set new value', async() => {
+    try{
+      await htlcInstProxy.setEconomics(ADDRESS_TM,ADDRESS_SMGADMIN,ratioPrecise);
+    }catch(err){
+      assert.fail(err.toString());
+    }
+  });
+
+  it('Others getEconomics  ==>check new value', async() => {
+    try {
+      let ret = await htlcInstProxy.getEconomics();
+      assert.equal(ret[0], ADDRESS_TM);
+      assert.equal(ret[1], ADDRESS_SMGADMIN);
+      assert.equal(ret[2].toNumber(), ratioPrecise);
+    } catch (err) {
+      assert.fail(err.toString());
+    }
+  });
+
+  it('Others setEconomics  ==>restore the saved address', async() => {
+    try{
+      await htlcInstProxy.setEconomics(tmAddress,smgAddress,revokeFeeRatio);
+    }catch(err){
+      assert.fail(err.toString());
+    }
+  });
+
+  it('Proxy   -> get the implementation address', async() => {
+    let address;
+    try{
+      let htlcProxy          = await HTLCProxy.deployed();
+      address = await htlcProxy.implementation();
+    }catch(err){
+      assert.fail(err.toString());
+    }
+    assert.equal(address,htlcInst.address);
+  });
+
+  it('Proxy   -> upgradeTo', async() => {
+    let address;
+    try{
+      let htlcProxy          = await HTLCProxy.deployed();
+      await htlcProxy.upgradeTo(ADDRESS_HTLC_PROXY_IMPL);
+      address = await htlcProxy.implementation();
+      assert.equal(address,ADDRESS_HTLC_PROXY_IMPL);
+    }catch(err){
+      assert.fail(err.toString());
+    }
+  });
+
+  it('Proxy   -> upgradeTo with the same implementation address', async() => {
+    let address;
+    try{
+      let htlcProxy          = await HTLCProxy.deployed();
+      await htlcProxy.upgradeTo(ADDRESS_HTLC_PROXY_IMPL);
+      address = await htlcProxy.implementation();
+      assert.equal(address,ADDRESS_HTLC_PROXY_IMPL);
+    }catch(err){
+      assert.include(err.toString(),"Cannot upgrade to the same implementation");
+    }
+  });
+
+  it('Proxy   -> upgradeTo with 0x address', async() => {
+    let address;
+    try{
+      let htlcProxy          = await HTLCProxy.deployed();
+      await htlcProxy.upgradeTo(ADDRESS_0);
+      address = await htlcProxy.implementation();
+      assert.equal(address,ADDRESS_0);
+    }catch(err){
+      assert.include(err.toString(),"Cannot upgrade to invalid address");
+    }
+  });
+
+  it('Proxy   -> restore', async() => {
+    let address;
+    try{
+      let htlcProxy          = await HTLCProxy.deployed();
+      await htlcProxy.upgradeTo(htlcInst.address);
+      address = await htlcProxy.implementation();
+      assert.equal(address,htlcInst.address);
+    }catch(err){
+      assert.fail(err.toString());
+    }
+  });
+
   ///
  ///==========================================================================================
  ///EOS->WAN
@@ -288,6 +388,7 @@ contract('Test HTLC', async (accounts) => {
       assert.fail(err);
     }
   });
+
   it('EOS->WAN inSmgLock  ==>Quota is not enough', async() => {
     try{
       // accounts[1] is the wan address of the user.
@@ -308,6 +409,7 @@ contract('Test HTLC', async (accounts) => {
       assert.include(err.toString(),"Quota is not enough");
     }
   });
+
   it('EOS->WAN inUserRedeem ==> redeem from not the receiver', async() => {
     try{
       // accounts[1] is the wan address of the user.
@@ -324,15 +426,8 @@ contract('Test HTLC', async (accounts) => {
     try{
       await htlcInstProxy.inUserRedeem(htlcUserRedeemParams.tokenOrigAccount,
         htlcUserRedeemParams.x, {from:accounts[1]});
-      // let ret = await tmInstProxy.getTokenInfo(tokenInfo.tokenOrigAccount);
-      //
-      // //check use has get the WEOS redeemed.
-      // let wanTokenSCAddr = ret[3];
-      // let wanTokenScInst = await WanToken.at(wanTokenSCAddr);
-      // let balance = await wanTokenScInst.balanceOf(accounts[1]);
       let balance = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
       assert.equal(balance,htlcSmgLockParams.value,"Redeemed token is not equal the locked value");
-
     }catch(err){
       assert.fail(err);
     }
@@ -422,7 +517,51 @@ contract('Test HTLC', async (accounts) => {
       assert.fail("should catch a error!")
     }
   });
+  it('WAN->EOS outUserLock  ==> Transferred fee is not enough', async() => {
 
+    let result=null;
+    try{
+      let balanceBeforeLock = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
+      let ret = await tmInstProxy.getTokenInfo(tokenInfo.tokenOrigAccount);
+      //check use has get the WEOS redeemed.
+      let wanTokenSCAddr = ret[3];
+      let wanTokenScInst = await WanToken.at(wanTokenSCAddr);
+      await wanTokenScInst.approve(htlcInstProxy.address,0,{from:accounts[1]});
+      await wanTokenScInst.approve(htlcInstProxy.address,appproveValue,{from:accounts[1]});
+      let balanceAfterLock = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
+
+      assert.equal(balanceBeforeLock, balanceAfterLock,"Approve should not withdraw the token!");
+
+
+      //let txFee = (new BN(htlcUserLockParams.value)).mul(new BN(addSmgParams.txFeeRatio)).div(new BN(DEFAULT_PRECISE)).sub(new BN(1));
+      let txFee = (new BN(htlcUserLockParams.value))
+        .div(new BN(10).pow(new BN(tokenInfo.decimals)))
+        .mul(new BN(10).pow(new BN(18)))
+        .mul(new BN(tokenInfo.token2WanRatio))
+        .mul(new BN(addSmgParams.txFeeRatio))
+        .div(new BN(DEFAULT_PRECISE))
+        .div(new BN(DEFAULT_PRECISE));
+      let txFeeNotEnough = txFee.sub(new BN(1));
+
+
+      let htlcUserLockParamsTemp = Object.assign({},htlcUserLockParams);
+      htlcUserLockParamsTemp.xHash = xHash10;
+      htlcUserLockParamsTemp.userOrigAccount = accounts[3];
+
+      await htlcInstProxy.outUserLock(htlcUserLockParamsTemp.xHash,
+        htlcUserLockParamsTemp.value,
+        htlcUserLockParamsTemp.tokenOrigAccount,
+        htlcUserLockParamsTemp.userOrigAccount,
+        htlcUserLockParamsTemp.storemanGroupPK, {from:accounts[1],value:txFeeNotEnough});
+
+    }catch(err){
+      result = err;
+      assert.include(err.toString(),"Transferred fee is not enough");
+    }
+    if(!result){
+      assert.fail("should Transferred fee is not enough")
+    }
+  });
   it('WAN->EOS outUserLock  ==> success', async() => {
     try{
       //  lock before approve
@@ -432,21 +571,33 @@ contract('Test HTLC', async (accounts) => {
       let wanTokenSCAddr = ret[3];
       let wanTokenScInst = await WanToken.at(wanTokenSCAddr);
 
+      await wanTokenScInst.approve(htlcInstProxy.address,0,{from:accounts[1]});
       await wanTokenScInst.approve(htlcInstProxy.address,appproveValue,{from:accounts[1]});
       let balanceAfterLock = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
+      assert.equal(balanceBeforeLock, balanceAfterLock,"Approve should not withdraw the token!");
 
-      assert.equal(balanceBeforeLock, balanceAfterLock,"Approve should not withdraw the token!")
+      let txFee = (new BN(htlcUserLockParams.value))
+        .div(new BN(10).pow(new BN(tokenInfo.decimals)))
+        .mul(new BN(10).pow(new BN(18)))
+        .mul(new BN(tokenInfo.token2WanRatio))
+        .mul(new BN(addSmgParams.txFeeRatio))
+        .div(new BN(DEFAULT_PRECISE))
+        .div(new BN(DEFAULT_PRECISE));
 
+      // console.log("After approved:"+balanceAfterLock);
+      // console.log("userLockValue:"+htlcUserLockParams.value.toString());
+      // console.log("txFee:"+txFee.toString());
 
       htlcUserLockParams.userOrigAccount = accounts[3];
       await htlcInstProxy.outUserLock(htlcUserLockParams.xHash,
         htlcUserLockParams.value,
         htlcUserLockParams.tokenOrigAccount,
         htlcUserLockParams.userOrigAccount,
-        htlcSmgLockParams.storemanGroupPK, {from:accounts[1]});
-      balanceAfterLock  = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
-      assert.equal(balanceAfterLock, balanceBeforeLock-htlcUserLockParams.value,"After lock, the balance is not right!")
+        htlcSmgLockParams.storemanGroupPK, {from:accounts[1],value:htlcUserLockParams.value});
 
+      balanceAfterLock  = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
+      // The WEOS value changed, not include the fee. Fee is subtracted from the value of the transaction. unit is WAN.
+      assert.equal(balanceAfterLock, (new BN(balanceBeforeLock)).sub(new BN(htlcUserLockParams.value)),"After lock, the balance is not right!")
     }catch(err){
       assert.fail(err);
     }
@@ -488,6 +639,7 @@ contract('Test HTLC', async (accounts) => {
       let wanTokenSCAddr = ret[3];
       let wanTokenScInst = await WanToken.at(wanTokenSCAddr);
 
+      await wanTokenScInst.approve(htlcInstProxy.address,0,{from:accounts[1]});
       await wanTokenScInst.approve(htlcInstProxy.address,appproveValue,{from:accounts[1]});
       let balanceAfterLock = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
 
@@ -502,7 +654,7 @@ contract('Test HTLC', async (accounts) => {
         htlcUserLockParamsTemp.value,
         htlcUserLockParamsTemp.tokenOrigAccount,
         htlcUserLockParamsTemp.userOrigAccount,
-        htlcUserLockParamsTemp.storemanGroupPK, {from:accounts[1]});
+        htlcUserLockParamsTemp.storemanGroupPK, {from:accounts[1],value:htlcUserLockParams.value});
 
       balanceAfterLock  = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
       assert.equal(balanceAfterLock, balanceBeforeLock-htlcUserLockParamsTemp.value,"After lock, the balance is not right!");
@@ -528,60 +680,20 @@ contract('Test HTLC', async (accounts) => {
       assert.include(err.toString(), "Revoke is not permitted");
     }
   });
+
   it('Others outUserRevoke  ==>success', async() => {
     // revoke
     try{
       let balanceBeforeRevoke = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
       await sleep(2*lockedTime);
-      await htlcInstProxy.outUserRevoke(tokenInfo.tokenOrigAccount,xHash4);
+      await debug(htlcInstProxy.outUserRevoke(tokenInfo.tokenOrigAccount,xHash4));
       let balanceAfterRevoke = await getValueFromContract(tokenInfo.tokenOrigAccount,accounts[1]);
-      assert.equal(balanceAfterRevoke, balanceBeforeRevoke+v3,"The balance of revoked is not right!");
+      assert.equal(balanceAfterRevoke, (new BN(balanceBeforeRevoke)).add(v3),"The balance of revoked is not right!");
     }catch(err){
       assert.fail(err.toString());
     }
   });
 
-  let tmAddress, smgAddress, precise;
-  it('WAN->EOS getEconomics  ==>The default value', async() => {
-    try{
-      let ret = await htlcInstProxy.getEconomics();
-      tmAddress = ret[0];
-      smgAddress = ret[1];
-      precise = ret[2];
-      assert.notEqual(ret[0], ADDRESS_0);
-      assert.notEqual(ret[1], ADDRESS_0);
-      assert.equal(ret[2].toNumber(), 0);
-    }catch(err){
-      assert.fail(err.toString());
-    }
-  });
-
-  it('WAN->EOS setEconomics  ==>set new value', async() => {
-    try{
-      await htlcInstProxy.setEconomics(ADDRESS_TM,ADDRESS_SMGADMIN,ratioPrecise);
-    }catch(err){
-      assert.fail(err.toString());
-    }
-  });
-
-  it('WAN->EOS getEconomics  ==>check new value', async() => {
-    try {
-      let ret = await htlcInstProxy.getEconomics();
-      assert.equal(ret[0], ADDRESS_TM);
-      assert.equal(ret[1], ADDRESS_SMGADMIN);
-      assert.equal(ret[2].toNumber(), ratioPrecise);
-    } catch (err) {
-      assert.fail(err.toString());
-    }
-  });
-
-    it('WAN->EOS setEconomics  ==>restore the saved address', async() => {
-      try{
-        await htlcInstProxy.setEconomics(tmAddress,smgAddress,precise);
-      }catch(err){
-        assert.fail(err.toString());
-      }
-  });
 
   it('Other deactivateStoremanGroup  ==>Only storeman group admin sc can call it', async() => {
     try{
@@ -711,6 +823,7 @@ contract('Test HTLC', async (accounts) => {
       htlcSmgLockParamsTemp.wanAddr = accounts[1];
       htlcSmgLockParamsTemp.xHash = xHash7;
       htlcSmgLockParamsTemp.storemanGroupPK = srcDebtStoremanPK;
+      htlcSmgLockParamsTemp.value = v4;
       await htlcInstProxy.inSmgLock(htlcSmgLockParamsTemp.tokenOrigAccount,
         htlcSmgLockParamsTemp.xHash,
         htlcSmgLockParamsTemp.wanAddr,
@@ -729,6 +842,7 @@ contract('Test HTLC', async (accounts) => {
       await smgInstProxy.smgApplyUnregisterByDelegate(tokenInfo.tokenOrigAccount,srcDebtStoremanPK, {from: accounts[4]});
 
       // debtLock
+      htlcDebtLockParamsTemp.value = v4;
       await htlcInstProxy.inDebtLock(tokenInfo.tokenOrigAccount,
         htlcDebtLockParamsTemp.xHash,
         htlcDebtLockParamsTemp.value,
@@ -797,6 +911,7 @@ contract('Test HTLC', async (accounts) => {
       htlcSmgLockParamsTemp.wanAddr = accounts[1];
       htlcSmgLockParamsTemp.xHash = xHash9;
       htlcSmgLockParamsTemp.storemanGroupPK = srcDebtStoremanPK1;
+      htlcSmgLockParamsTemp.value = v4;
       await htlcInstProxy.inSmgLock(htlcSmgLockParamsTemp.tokenOrigAccount,
         htlcSmgLockParamsTemp.xHash,
         htlcSmgLockParamsTemp.wanAddr,
@@ -818,6 +933,7 @@ contract('Test HTLC', async (accounts) => {
       let htlcDebtLockParamsTemp = Object.assign({},htlcDebtLockParams);
       htlcDebtLockParamsTemp.srcStoremanPK = srcDebtStoremanPK1;
       htlcDebtLockParamsTemp.xHash = xHash11;
+      htlcDebtLockParamsTemp.value = v4;
       await htlcInstProxy.inDebtLock(tokenInfo.tokenOrigAccount,
         htlcDebtLockParamsTemp.xHash,
         htlcDebtLockParamsTemp.value,
@@ -858,5 +974,5 @@ async function getValueFromContract(tokenOrigAccount, userAccountAddress){
   let wanTokenSCAddr = ret[3];
   let wanTokenScInst = await WanToken.at(wanTokenSCAddr);
   let balance = await wanTokenScInst.balanceOf(userAccountAddress);
-  return balance.toNumber();
+  return balance.toString();
 };
