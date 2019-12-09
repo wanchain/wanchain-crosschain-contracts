2242b049238ab0f5b849d0d3d2408466ad41b1da
Gabriel Guo <pkguowu@gmail.com>
2019-12-09 15:29:40 +0800

    modify beforeTest.sh to be compatible for macOS

--- eos-wan-cross/solidity/test/beforeTest.sh
+++ eos-wan-cross/solidity/test/beforeTest.sh
@@ -1,20 +1,18 @@
-curPath=$(dirname "$0")
+curPath=$(cd `dirname $0`; pwd)
 cd $curPath
 
 if [ ! -f "../contracts/htlc/lib/HTLCLib.sol.org" ];then
 	cp ../contracts/htlc/lib/HTLCLib.sol ../contracts/htlc/lib/HTLCLib.sol.org
 fi
 
-#cp ../contracts/htlc/lib/HTLCLib.sol ../contracts/htlc/lib/HTLCLib.sol.org
-sed -i 's/DEF_LOCKED_TIME[ \t]*=.*\;/DEF_LOCKED_TIME = 6;/' ../contracts/htlc/lib/HTLCLib.sol
+perl -pi -e 's/DEF_LOCKED_TIME[ \t]*=.*/DEF_LOCKED_TIME = 6/' ../contracts/htlc/lib/HTLCLib.sol
 
 if [ ! -f "../contracts/htlc/lib/commonLib.sol.org" ];then
 	cp ../contracts/htlc/lib/commonLib.sol ../contracts/htlc/lib/commonLib.sol.org
 fi
-#cp ../contracts/htlc/lib/commonLib.sol ../contracts/htlc/lib/commonLib.sol.org
-sed -i '/.*SchnorrVerifier\.verify/d'  ../contracts/htlc/lib/commonLib.sol
+perl -ni -e 'print unless /.*SchnorrVerifier\.verify/'  ../contracts/htlc/lib/commonLib.sol
 
 if [ ! -f "../contracts/tokenManager/TokenManagerStorage.sol.org" ];then
 	cp ../contracts/tokenManager/TokenManagerStorage.sol ../contracts/tokenManager/TokenManagerStorage.sol.org
 fi
-sed -i 's/MIN_WITHDRAW_WINDOW[ \t]*=.*\;/MIN_WITHDRAW_WINDOW = 10;/' ../contracts/tokenManager/TokenManagerStorage.sol
+perl -pi -e 's/MIN_WITHDRAW_WINDOW[ \t]*=.*\;/MIN_WITHDRAW_WINDOW = 10;/' ../contracts/tokenManager/TokenManagerStorage.sol
