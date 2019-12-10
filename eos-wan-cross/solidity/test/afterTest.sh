75a47f91ed729e1dee945037c51d30b2e89fd34c
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2019-12-10 16:32:28 +0800

    add schnorr signature,format code,change code by contract review comments

--- eos-wan-cross/solidity/test/afterTest.sh
+++ eos-wan-cross/solidity/test/afterTest.sh
@@ -2,6 +2,6 @@ curPath=$(dirname "$0")
 cd $curPath
 
 mv  ../contracts/htlc/lib/HTLCLib.sol.org ../contracts/htlc/lib/HTLCLib.sol
-mv  ../contracts/htlc/lib/commonLib.sol.org ../contracts/htlc/lib/commonLib.sol
+#mv  ../contracts/htlc/lib/commonLib.sol.org ../contracts/htlc/lib/commonLib.sol
 
 mv  ../contracts/tokenManager/TokenManagerStorage.sol.org ../contracts/tokenManager/TokenManagerStorage.sol
