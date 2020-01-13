4d229789333870712b5a521375907e6702fbc17a
zhaoxiaofeng <zhao.xiaofeng@wanglutech.com>
2020-01-13 17:43:50 +0800

    fix compile error

--- eos-wan-cross/c++/schnorr.verification/compile.sh
+++ eos-wan-cross/c++/schnorr.verification/compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-eosio-cpp -I . src/schnorr.verification.cpp src/Utils.cpp src/FieldInt.cpp src/CurvePoint.cpp src/Uint256.cpp -o schnorr.verification.wasm -abigen
+eosio-cpp -I . src/Schnorr.verification.cpp src/Utils.cpp src/FieldInt.cpp src/CurvePoint.cpp src/Uint256.cpp -o schnorr.verification.wasm -abigen
